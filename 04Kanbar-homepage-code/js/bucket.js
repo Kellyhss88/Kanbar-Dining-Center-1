b66a26831be86273745e02dab20d6f39dfe707e0
shanshanhan <kellyhss88@gmail.com>
2019-12-17 23:07:59 -0500

    added design and code

---
+++ 04Kanbar-homepage-code/js/bucket.js
@@ -0,0 +1,18 @@
+
+
+
+    // Your JS below this line...
+    $(document).ready(function(){
+      $('.carousel').slick({
+      slidesToShow: 4,
+      dots:true,
+      centerMode: false,
+      });
+    });
+    // $(document).ready(function(){
+    //   $('.carousel2').slick({
+    //   slidesToShow: 5,
+    //   dots:false,
+    //   centerMode: false,
+    //   });
+    // });
